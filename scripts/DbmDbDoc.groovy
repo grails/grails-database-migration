b3285a279a37441ff1514ed030bb08c682c1aedb
Burt Beckwith <burt@burtbeckwith.com>
2010-11-28 18:12:28 -0500

    db-doc supports contexts

--- scripts/DbmDbDoc.groovy
+++ scripts/DbmDbDoc.groovy
@@ -23,7 +23,7 @@ target(dbmDbDoc: 'Generates Javadoc-like documentation based on current database
 	depends dbmInit
 
 	doAndClose {
-		liquibase.generateDocumentation argsList[0] ?: MigrationUtils.dbDocLocation
+		liquibase.generateDocumentation argsList[0] ?: MigrationUtils.dbDocLocation, contexts
 	}
 }
 
