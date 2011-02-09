ba4fd8e1b804aa070642c4fe721f1268552cf2bb
Burt Beckwith <burt@burtbeckwith.com>
2011-02-09 00:21:02 -0500

    GRAILSPLUGINS-2751

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,3 +1,4 @@
+// sets the scriptName so we know if we cna automigrate
 eventPackageAppEnd = {
 	try {
 		def MigrationUtils = classLoader.loadClass('grails.plugin.databasemigration.MigrationUtils')
@@ -5,5 +6,18 @@ eventPackageAppEnd = {
 	}
 	catch (Throwable t) {
 		println "\nERROR setting MigrationUtils.scriptName, auto-migrate not possible: $e.message\n"
+	}
+}
+
+// copies migration files to the classpath so they'll be available for automigration
+eventCreateWarStart = { name, stagingDir ->
+
+	def conf = config.grails.plugin.databasemigration
+	String changelogLocation = conf.changelogLocation ?: 'grails-app/migrations'
+	File classesDir = new File(stagingDir, 'WEB-INF/classes/migrations')
+	classesDir.mkdirs()
+
+	ant.copy(todir: classesDir.path, verbose: true) {
+		fileset dir: changelogLocation
 	}
 }
