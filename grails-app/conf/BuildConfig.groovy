4b60ab169fc06955e3643c803c52f3be1f4bf5de
Burt Beckwith <burt@burtbeckwith.com>
2011-08-17 02:00:45 -0400

    doc updates for 1.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
 grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
-grails.project.docs.output.dir = 'docs' // docs are checked into gh-pages branch
+grails.project.docs.output.dir = 'docs/manual' // docs are checked into gh-pages branch
 
 grails.project.dependency.resolution = {
 
