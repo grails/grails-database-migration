ea69df966b37f143e634b38e575af4f2b02fa7f2
Burt Beckwith <burt@burtbeckwith.com>
2011-10-10 23:34:11 -0400

    update to 2.0 M2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,11 @@
 grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
-grails.project.docs.output.dir = 'docs' // docs are checked into gh-pages branch
+grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
 grails.project.dependency.resolution = {
 
 	inherits 'global'
-
 	log 'warn'
 
 	repositories {
@@ -14,6 +13,7 @@ grails.project.dependency.resolution = {
 		grailsHome()
 		grailsCentral()
 
+		mavenLocal()
 		mavenCentral()
 	}
 
