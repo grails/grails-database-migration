0f4989cb674dbc171d4704111632047d8cb25299
Predrag Knezevic <predrag.knezevic@abz-reporting.com>
2011-11-08 15:25:34 +0100

    added grails.plugin.databasemigration.forceAutoMigrate param that can be used for forcing auto migrate, no matter how the application is started

--- src/groovy/grails/plugin/databasemigration/MigrationUtils.groovy
+++ src/groovy/grails/plugin/databasemigration/MigrationUtils.groovy
@@ -141,7 +141,7 @@ class MigrationUtils {
 	static boolean canAutoMigrate() {
 
 		// in a war
-		if (application.warDeployed) {
+		if (application.warDeployed || getConfig()?.forceAutoMigrate) {
 			return true
 		}
 
