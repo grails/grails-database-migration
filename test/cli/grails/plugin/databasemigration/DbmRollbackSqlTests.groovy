b606f7e0d4f0dc65017fc02f53b5bcbcabbc9093
Zan Thrash <zanthrash@gmail.com>
2012-05-15 14:07:34 -0500

    Make DbmRollbackSql script work with multiple dataSources. Added multiple datasource tests as well.

--- test/cli/grails/plugin/databasemigration/DbmRollbackSqlTests.groovy
+++ test/cli/grails/plugin/databasemigration/DbmRollbackSqlTests.groovy
@@ -20,6 +20,7 @@ package grails.plugin.databasemigration
 class DbmRollbackSqlTests extends AbstractScriptTests {
 
 	void testRollbackSql() {
+        def url = AbstractScriptTests.URL
 
 		// run all the updates
 		copyTestChangelog()
@@ -27,7 +28,7 @@ class DbmRollbackSqlTests extends AbstractScriptTests {
 
 		// manually tag the first update
 		String tagName = 'THE_TAG'
-		executeUpdate "update databasechangelog set tag=? where id='test-1'", [tagName]
+        executeUpdate url, "update databasechangelog set tag=? where id='test-1'", [tagName]
 
 		// test parameter check
 		executeAndCheck(['dbm-rollback-sql'], false)
@@ -41,4 +42,30 @@ class DbmRollbackSqlTests extends AbstractScriptTests {
 		assertTrue output.contains("DELETE FROM DATABASECHANGELOG  WHERE ID='test-2' AND AUTHOR='burt' AND FILENAME='changelog.cli.test.groovy'")
 		assertTrue output.contains("DELETE FROM DATABASECHANGELOG  WHERE ID='test-3' AND AUTHOR='burt' AND FILENAME='changelog.cli.test.groovy'")
 	}
+
+
+    void testRollbackSqlForSecondaryDataSource() {
+        def url = AbstractScriptTests.SECONDARY_URL
+
+   		// run all the updates
+   		copyTestChangelog('test.changelog', SECONDARY_TEST_CHANGELOG)
+   		executeAndCheck (['dbm-update', '--dataSource=secondary'])
+
+   		// manually tag the first update
+   		String tagName = 'THE_TAG'
+           executeUpdate url, "update databasechangelog set tag=? where id='test-1'", [tagName]
+
+   		// test parameter check
+   		executeAndCheck(['dbm-rollback-sql', '--dataSource=secondary'], false)
+   		assertTrue output.contains('ERROR: The dbm-rollback-sql script requires a tag')
+
+   		executeAndCheck(['dbm-rollback-sql', tagName, '--dataSource=secondary'])
+
+   		assertTrue output.contains('ALTER TABLE PERSON DROP COLUMN ZIPCODE')
+   		assertTrue output.contains('ALTER TABLE PERSON DROP COLUMN STREET1')
+   		assertTrue output.contains('ALTER TABLE PERSON DROP COLUMN STREET2')
+   		assertTrue output.contains("DELETE FROM DATABASECHANGELOG  WHERE ID='test-2' AND AUTHOR='burt' AND FILENAME='changelog.cli.secondary-test.groovy'")
+   		assertTrue output.contains("DELETE FROM DATABASECHANGELOG  WHERE ID='test-3' AND AUTHOR='burt' AND FILENAME='changelog.cli.secondary-test.groovy'")
+   	}
+
 }
