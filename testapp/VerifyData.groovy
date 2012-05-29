083fc0e13586d97e35adf9ffbb05c3d484bf1df4
Zan Thrash <zanthrash@gmail.com>
2012-05-29 09:01:17 -0500

    Refresh the orders object graph to prevent LazyLoadingException

--- testapp/VerifyData.groovy
+++ testapp/VerifyData.groovy
@@ -23,27 +23,36 @@ target(verifyData: 'Verifies data') {
 		assertEquals 30000, products[2].price
 
 		assertEquals 3, Order.count()
-		def orders = Order.listOrderById()
+        def orders = Order.listOrderById()
 
-		assertEquals 'c1', orders[0].customer.name
-		assertEquals 1, orders[0].items.size()
-		def items = orders[0].items.sort { it.product.name }
-		assertEquals 'p1', items[0].product.name
-		assertEquals 1, items[0].quantity
+        orders.each { order ->
+            order.refresh()
+            order.customer.refresh()
+            order.items.each{ item ->
+                item.refresh()
+                item.product.refresh()
+            }
+        }
 
-		assertEquals 'c1', orders[1].customer.name
-		assertEquals 2, orders[1].items.size()
-		items = orders[1].items.sort { it.product.name }
-		assertEquals 'p2', items[0].product.name
-		assertEquals 2, items[0].quantity
-		assertEquals 'p3', items[1].product.name
-		assertEquals 1, items[1].quantity
+        assertEquals 'c1', orders[0].customer.name
+        assertEquals 1, orders[0].items.size()
+        def items = orders[0].items.sort { it.product.name }
+        assertEquals 'p1', items[0].product.name
+        assertEquals 1, items[0].quantity
 
-		assertEquals 'c2', orders[2].customer.name
-		assertEquals 1, orders[2].items.size()
-		items = orders[2].items.sort { it.product.name }
-		assertEquals 'p1', items[0].product.name
-		assertEquals 5, items[0].quantity
+        assertEquals 'c1', orders[1].customer.name
+        assertEquals 2, orders[1].items.size()
+        items = orders[1].items.sort { it.product.name }
+        assertEquals 'p2', items[0].product.name
+        assertEquals 2, items[0].quantity
+        assertEquals 'p3', items[1].product.name
+        assertEquals 1, items[1].quantity
+
+        assertEquals 'c2', orders[2].customer.name
+        assertEquals 1, orders[2].items.size()
+        items = orders[2].items.sort { it.product.name }
+        assertEquals 'p1', items[0].product.name
+        assertEquals 5, items[0].quantity
 	}
 }
 
