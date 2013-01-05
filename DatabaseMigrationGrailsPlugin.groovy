30e10861062f8d6bbef8e2560fc42dc3a19be1c0
Burt Beckwith <beckwithb@vmware.com>
2013-01-04 23:01:37 -0500

    1.3.2 release

--- DatabaseMigrationGrailsPlugin.groovy
+++ DatabaseMigrationGrailsPlugin.groovy
@@ -41,7 +41,7 @@ import liquibase.sqlgenerator.core.CreateTableGenerator
 
 class DatabaseMigrationGrailsPlugin {
 
-	String version = '1.3.1'
+	String version = '1.3.2'
 	String grailsVersion = '1.3.0 > *'
 	String author = 'Burt Beckwith'
 	String authorEmail = 'beckwithb@vmware.com'
