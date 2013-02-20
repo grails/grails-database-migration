4ddbd659489b1c72adf11fbbab0b9c39fffeacd6
Svetozar Misljencevic <svetozar01@gmail.com>
2013-02-20 15:48:21 -0500

    minor internal changes

--- DatabaseMigrationGrailsPlugin.groovy
+++ DatabaseMigrationGrailsPlugin.groovy
@@ -41,7 +41,7 @@ import liquibase.sqlgenerator.core.CreateTableGenerator
 
 class DatabaseMigrationGrailsPlugin {
 
-	String version = '1.3.2'
+	String version = '1.3.2pg20'
 	String grailsVersion = '1.3.0 > *'
 	String author = 'Burt Beckwith'
 	String authorEmail = 'beckwithb@vmware.com'
