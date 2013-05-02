c4168fda3e0f744bcff2ca3c88df337bba6479d3
Burt Beckwith <burt@burtbeckwith.com>
2013-05-02 00:00:29 -0400

    updated to 2.1.5 and added the wrapper for a BuildHive build

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,5 +23,9 @@ grails.project.dependency.resolution = {
 		build(':release:2.2.1', ':rest-client-builder:1.0.3') {
 			export = false
 		}
+
+		runtime ":hibernate:$grailsVersion", {
+			export = false
+		}
 	}
 }
