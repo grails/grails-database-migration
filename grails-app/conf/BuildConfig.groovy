94d9681b6ae6255142a29b59c1208a00dc8e92f2
Lari Hotari <lhotari@gopivotal.com>
2014-04-15 13:51:56 +0300

    upgrade to Grails 2.3.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,30 +1,34 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
+grails.project.fork = false
+
+grails.project.dependency.resolver = "maven"
+
 grails.project.dependency.resolution = {
 
 	inherits 'global'
 	log 'warn'
 
 	repositories {
-		grailsCentral()
-
 		mavenLocal()
-		mavenCentral()
+		grailsCentral()
+		mavenRepo "http://repo.grails.org/grails/core"
 	}
 
 	dependencies {
 		compile('org.liquibase:liquibase-core:2.0.5') {
 			excludes 'junit', 'easymockclassextension', 'ant', 'servlet-api', 'spring'
 		}
+		test 'commons-dbcp:commons-dbcp:1.4'
 	}
 
 	plugins {
-		build(':release:2.2.1', ':rest-client-builder:1.0.3') {
+		build(':release:3.0.1', ':rest-client-builder:1.0.3') {
 			export = false
 		}
 
-		runtime ":hibernate:$grailsVersion", {
+		runtime ":hibernate:3.6.10.12", {
 			export = false
 		}
 	}
