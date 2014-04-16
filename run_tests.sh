a6b7bc9ccc63ff53f5344b43bf79d9fb9105dba7
Lari Hotari <lhotari@gopivotal.com>
2014-04-16 14:48:34 +0300

    run mysql test app only when --all option is given as an argument to the script

--- run_tests.sh
+++ run_tests.sh
@@ -6,5 +6,9 @@ grails compile
 grails maven-install
 grails test-app :integration
 grails test-app :cli
-cd testapp
-./run_test_app.sh
+set +e +x
+if [[ "$1" == "--all" ]]; then
+	echo "Running mysql testapp"
+	cd testapp
+	./run_test_app.sh
+fi
