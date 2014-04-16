dbc255f87d3bea9093273dc51feaaa882e7bfdf0
Lari Hotari <lhotari@gopivotal.com>
2014-04-16 11:05:07 +0300

    fix integration tests

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,13 +30,21 @@ environments {
 	}
 	test {
 		dataSource {
-			dbCreate = 'update'
-			url = 'jdbc:h2:mem:testDb'
+			url = 'jdbc:h2:file:target/testdb'
+		}
+
+		dataSource_secondary {
+			pooled = true
+			driverClassName = 'org.h2.Driver'
+			username = 'sa'
+			password = ''
+			url = 'jdbc:h2:file:target/testdb-secondary'
+			dialect = org.hibernate.dialect.H2Dialect
 		}
 	}
 	dbdiff {
 		dataSource {
-			url = 'jdbc:h2:file:target/dbdiff/dbdiff'
+			url = 'jdbc:h2:file:file:target/dbdiff'
 		}
 	}
 }
