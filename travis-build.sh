27bc94e292d361475d6307bb4a5dc8bd4818be34
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2015-03-12 18:11:44 +0900

    update travis settings

--- travis-build.sh
+++ travis-build.sh
@@ -1,48 +1,21 @@
 #!/bin/bash
 set -e
 rm -rf *.zip
-./grailsw refresh-dependencies --non-interactive
-./run_tests.sh -c 
-./grailsw package-plugin --non-interactive
-./grailsw doc --pdf --non-interactive
+./gradlew clean test assemble
 
-filename=$(find . -name "grails-*.zip" | head -1)
-filename=$(basename $filename)
-plugin=${filename:7}
-plugin=${plugin/.zip/}
-plugin=${plugin/-SNAPSHOT/}
-version="${plugin#*-}"; 
-plugin=${plugin/"-$version"/}
+filename=$(find build/libs -name "*.jar" | head -1)
+filename=$(basename "$filename")
 
-echo "Publishing plugin $plugin with version $version"
+EXIT_STATUS=0
+echo "Publishing archives for branch $TRAVIS_BRANCH"
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "grails-plugins/grails-database-migration" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-  git config --global user.name "$GIT_NAME"
-  git config --global user.email "$GIT_EMAIL"
-  git config --global credential.helper "store --file=~/.git-credentials"
-  echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+  echo "Publishing archives"
 
-
-  if [[ $filename != *-SNAPSHOT* ]]
-  then
-    git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
-    cd gh-pages
-    git rm -rf .
-    cp -r ../target/docs/. ./
-    git add *
-    git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-    git push origin HEAD
-    cd ..
-    rm -rf gh-pages
-  else
-    echo "SNAPSHOT version, not publishing docs"
+  if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
   fi
 
-
-  ./grailsw publish-plugin --no-scm --allow-overwrite --non-interactive
-else
-  echo "Not on master branch, so not publishing"
-  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
-  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 fi
+
+exit $EXIT_STATUS
