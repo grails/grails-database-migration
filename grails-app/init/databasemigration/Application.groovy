132df2f8b22868d48e26ac775d8ac225aa1aa362
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2015-03-13 14:23:27 +0900

    add integration tests

--- grails-app/init/databasemigration/Application.groovy
+++ grails-app/init/databasemigration/Application.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.grails.plugins.databasemigration
+package databasemigration
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
