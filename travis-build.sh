7e59df6950b1cf38ed1ffb2eb6310799051f468b
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2015-03-15 14:45:19 +0900

    update travis settings

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 rm -rf *.zip
-./gradlew clean test assemble
+./gradlew clean test integrationTest assemble
 
 filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
