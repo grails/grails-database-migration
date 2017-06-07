6665f1dc274519236558d238d8716924c780fd69
sdelamo <sergio.delamo@softamo.com>
2017-06-07 15:19:30 +0200

    Publish docs

--- travis-build.sh
+++ travis-build.sh
@@ -12,8 +12,8 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
 
   echo "Publishing archives"
 
-  #if [[ -n $TRAVIS_TAG ]]; then
-      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  # if [[ -n $TRAVIS_TAG ]]; then
+  #    ./gradlew bintrayUpload || EXIT_STATUS=$?
   #else
   #    ./gradlew publish || EXIT_STATUS=$?
   #fi
